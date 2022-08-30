19820626c4d3785db95cf985fee3881083eebdb8
Bernardita Arauco <bernarditaarauco@gmail.com>
2022-08-29 21:22:38 -0300

    .

---
+++ segundo-desafio/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+    </div>
+    <React.Fragment>
+      <NavBar/>
+    </React.Fragment>
+    </>
+  );
+}
+
+export default App;
